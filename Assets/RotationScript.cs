edb4c0a6c4c2a96bbbad14ce2a8f680ceb209fb5
jscotty <jscotty@hotmail.nl>
2015-01-26 13:48:09 +0100

    player movement

---
+++ Assets/RotationScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+	
+	private float rot = 0f;
+	public float rotSpeed = 500f;
+
+	void Update () {
+		
+		rot = Input.GetAxis ("RightJoystickX");
+		
+		transform.Rotate(new Vector3(0f,0f,rot) * -1 * rotSpeed * Time.deltaTime);
+	}
+}
